38df122d953de454d94d2b4b4a4868242467f8b4
Crynowecer0 <crynowecer11@gmail.com>
2023-01-14 14:30:55 -0500

    created Models folder, and empty skeleton nps_api model file

--- server/Routes/nps_api.js
+++ server/Routes/nps_api.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
+
+require('dotenv').config()
 
 router.get('/', async(req, res)=>{
   console.log('hi from the NPS route!');
