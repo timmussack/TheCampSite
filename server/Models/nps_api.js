65eb33d4b0afa2b0d28be01664a76c70013babba
Crynowecer0 <crynowecer11@gmail.com>
2023-01-14 16:26:39 -0500

    updated file structure on server, created one working query to nps api for campsites.

--- server/Models/nps_api.js
+++ server/Models/nps_api.js
@@ -1,3 +1,26 @@
 const axios = require('axios')
-require('dotenv').config()
+const path = require('node:path');
+require('dotenv').config({ path: require('find-config')('.env') })
 
+const npsBaseUrl = process.env.NPS_BASE_URL
+const npsAPIKEY = process.env.NPS_API_KEY
+
+console.log('NPS base url', process.env.NPS_BASE_URL)
+
+const getOne = (req, res) => {
+  const config = {
+      method: 'get',
+      url: `${npsBaseUrl}/campgrounds?Limit=1`,
+      headers: {
+        'x-api-key': npsAPIKEY
+      }
+    };
+    console.log('base variable', config.url)
+
+    axios(config)
+    .then((data)=>console.log(data.data))
+    .catch((error)=>console.log(error, 'error'))
+
+}
+
+module.exports.getOne = getOne;
--- server/Routes/nps_api.js
+++ server/Routes/nps_api.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const router = express.Router()
-const axios = require('axios')
+const { getOne } = require('../models/nps_api.js')
 
-require('dotenv').config()
-
-router.get('/', async(req, res)=>{
-  console.log('hi from the NPS route!');
-  res.send('hello from the nps router')
+//get a single site from the nps API
+router.get('/getOneSite', async(req, res)=>{
+  try {
+    const data = await getOne(req, res)
+    console.log('DATA FROM route', data)
+    res.send(data)
+  } catch(error) {
+    console.log(error)
+  }
 });
 
 module.exports = router
