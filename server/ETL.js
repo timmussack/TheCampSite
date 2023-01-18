15543e0de3f3dd18ed77aa008eb146fb1b90dc92
Willy McNamara <wmcnamara11@gmail.com>
2023-01-18 09:50:52 -0600

    Added id to ETL

--- server/ETL.js
+++ server/ETL.js
@@ -59,6 +59,7 @@ NPSModels.getAll().then((res) => {
     }
     /* ======= Create and save new document to mongo collection ======= */
     new MongoModels.Campsite({
+      id: i + 1,
       page: page,
       campsiteName: curr.name,
       campsitePhone: phoneNumber,
