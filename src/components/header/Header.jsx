93d5bce599eb3224466e7db7ca1738061fb4e191
Cordelia Payson <cordelia.payson@gmail.com>
2023-01-18 10:06:11 -0500

    setting up mobile filter bar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,14 +4,20 @@ import TitleLogo from './TitleLogo.jsx';
 import DistanceSelect from './DistanceSelect.jsx';
 import FilterBySelect from './FilterBySelect.jsx';
 import LogInIcon from './LogInIcon.jsx';
+import FilterBar from './FilterBar.jsx';
 
 function Header() {
   return (
     <div className="border-y-2 border-pop py-3 flex flex-wrap flex-row justify-around items-center sticky top-0 z-50 bg-neutral-50">
       <TitleLogo />
       <SearchBar />
+
+      {/* on mobile disappears */}
       <DistanceSelect />
+
+      {/* on mobile disappears */}
       <FilterBySelect />
+
       <LogInIcon />
     </div>
   );
