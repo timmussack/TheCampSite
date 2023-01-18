a8358c08d88c70207810ce9bd6605352383d7343
Cordelia Payson <cordelia.payson@gmail.com>
2023-01-17 20:18:21 -0500

    only 'log in' shows on mobile

--- src/components/header/LogInIcon.jsx
+++ src/components/header/LogInIcon.jsx
@@ -9,7 +9,10 @@ function LogInIcon() {
     <button className="cursor-pointer transform transition duration-500 hover:scale-110 mr-3" type="button" onClick={() => navigate('/login')}>
       <div className="flex flex-direction: row">
         <FaUserAlt size={20} />
-        <p className="ml-1">Log in or Sign up</p>
+
+        {/* text split so on mobile only "log in" shows */}
+        <p className="ml-1">Log in</p>
+        <p className="ml-1 hidden md:flex"> or Sign up</p>
       </div>
     </button>
 
