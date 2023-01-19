8226803e710a66a4017f9ab186c17cde8211633c
Tim Mussack <tim.mussack@gmail.com>
2023-01-19 16:35:55 -0600

    Update to Card.jsx

--- src/components/home/Card.jsx
+++ src/components/home/Card.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
