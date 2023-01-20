f09cfc46aa0c47eea13828653689fd3ffe18cbd3
Cordelia Payson <cordelia.payson@gmail.com>
2023-01-20 12:22:17 -0500

    add smaller icon for mobile

--- src/components/header/TitleLogo.jsx
+++ src/components/header/TitleLogo.jsx
@@ -8,7 +8,16 @@ function TitleLogo() {
       to="/"
     >
       <div className="flex flex-row items-center cursor-pointer">
-        <GiCampingTent size={50} />
+
+        {/* smaller icon for mobile */}
+        <div className="sm:hidden">
+          <GiCampingTent size={35} />
+        </div>
+
+        {/* larger icon for desktop */}
+        <div className="hidden sm:block">
+          <GiCampingTent size={50} />
+        </div>
         <h1 className="text-primary text-4xl font-bold hidden lg:flex">
           The
           <span className="text-pop">Camp</span>
